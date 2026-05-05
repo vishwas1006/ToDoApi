d9b9070eb7253025026868d21b9dfd5ee7895a34
vishwas1006 <vishwasgowda547@gmail.com>
2026-05-05 10:38:20 +0530

    Initial Commit

---
+++ Controllers/ToDoController.cs
@@ -0,0 +1,76 @@
+﻿using ToDoApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class ToDoController : ControllerBase
+    {
+        public static List<ToDoItem> tasks = new List<ToDoItem>
+        {
+            new ToDoItem
+            {
+                Id=1,
+                Title="Learn ASP Net",
+                IsCompleted=false
+            }
+        };
+
+        [HttpGet]
+
+        public ActionResult<IEnumerable<ToDoItem>> GetAll()
+        {
+            return Ok(tasks);
+        }
+
+        [HttpGet("{id}")]
+
+        public ActionResult<ToDoItem> GetById(int id)
+        {
+            var task = tasks.FirstOrDefault(t=>t.Id==id);
+
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(task);
+        }
+        [HttpPost]
+        public ActionResult<ToDoItem> Create(ToDoItem item)
+        {
+            item.Id = tasks.Count + 1;
+            tasks.Add(item);
+            return Ok(item);
+        }
+
+        [HttpPut("{id}")]
+
+        public ActionResult<ToDoItem> Update(int id,ToDoItem updatedItem)
+        {
+            var item = tasks.FirstOrDefault(x => x.Id == id);
+
+            if (item == null) return NotFound();
+
+            item.Title = updatedItem.Title;
+            item.IsCompleted = updatedItem.IsCompleted;
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+
+        public ActionResult<ToDoItem> Delete (int id)
+        {
+            var item = tasks.FirstOrDefault(x => x.Id == id); ;
+            if (item == null) return NotFound();
+
+            tasks.Remove(item);
+
+            return NoContent();
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+//var builder = WebApplication.CreateBuilder(args);
+
+//// Add services to the container.
+
+//builder.Services.AddControllers();
+//// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+//builder.Services.AddOpenApi();
+
+//var app = builder.Build();
+
+//// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.MapOpenApi();
+//}
+
+//app.UseHttpsRedirection();
+
+//app.UseAuthorization();
+
+//app.MapControllers();
+
+//app.Run();
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services
+builder.Services.AddControllers();
+
+// Add Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
